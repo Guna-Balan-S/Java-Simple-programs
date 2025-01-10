7f3966712025b0a316b89a7cf719f45584410ae6
S.Guna Balan <162681185+Guna-Balan-S@users.noreply.github.com>
2025-01-10 17:09:10 +0530

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,14 @@
+//Print 1 to n numbers
+import java.util.Scanner;
+class Print{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+        
+        
+    }
+}
